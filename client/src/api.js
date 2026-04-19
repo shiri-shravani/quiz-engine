e5a56ddbaa1512f40bc06b376372140afb8b6ed8
Shiri Shravani <vtu25054@veltech.edu.in>
2026-04-19 16:24:17 +0530

    fix cors and production api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://quiz-engine-backend-62zv.onrender.com',
 });
 
 api.interceptors.request.use(config => {
