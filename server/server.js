c23c9c577bac196e60bad9ff1586e1919bd2ac08
Shiri Shravani <vtu25054@veltech.edu.in>
2026-04-19 16:10:55 +0530

    fix cors and production api

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const seedQuestions = require('./data/seedQuestions');
 
 dotenv.config();
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: "https://quiz-engine-two.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDb().then(async () => {
